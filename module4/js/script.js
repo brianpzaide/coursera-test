9a3487212da8bfd833b0a161779ed1335ca1f0ce
Brian p Zaide <brianpzaide@gmail.com>
2018-11-28 08:09:20 +0530

    Assignment-Module4

--- module4/js/script.js
+++ module4/js/script.js
@@ -2,8 +2,7 @@
 ( function (names) {
 	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	for (var i = 0; i < names.length; i++) {
-		var firstLetter = names[i].charAt(0).toLowerCase();
-		if (firstLetter === 'j'){
+		if ((names[i].toLowerCase()).charAt(0) === 'j'){
 			byeSpeaker.speak(names[i]);
 		}else{
 			helloSpeaker.speak(names[i]);
