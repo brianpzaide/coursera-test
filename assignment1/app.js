1b3de0b9d0e7314713a69a296ae919f66284f501
Brian p Zaide <brianpzaide@gmail.com>
2018-12-19 08:48:52 +0530

    fullstack_course_5_Assignment1.

---
+++ assignment1/app.js
@@ -0,0 +1,47 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.items = "";
+  function noSpaces(a) {
+      return a != " ";
+  }
+  function noEmpties(a) {
+    return a != "";
+  }
+  $scope.checkiftoomuch = function (){
+    var lchars = $scope.items.split(',');
+    for(let i=0; i < lchars.length; i++){
+      let alpha = lchars[i].split(" ");
+      let x= "";
+      for(let j=0; j < alpha.length; j++){
+        if(alpha[j]!=""){
+        x += alpha[j];
+      }
+    }
+    lchars[i] = x;
+  }
+  lchars = lchars.filter(noSpaces);
+  lchars = lchars.filter(noEmpties);
+  if (lchars.length > 3){
+    $scope.Mychars = lchars;
+    $scope.MyMessage = "Too much!";
+    $scope.m_State = "g"
+  }
+  if (lchars.length === 0){
+    $scope.Mychars = "No Proper Items";
+    $scope.MyMessage = "Please enter data first";
+    $scope.m_State = "r"
+  }
+  else {
+    $scope.Mychars = lchars;
+    $scope.MyMessage = "Enjoy!";
+    $scope.m_State = "g"
+  }
+}
+}
+})();
