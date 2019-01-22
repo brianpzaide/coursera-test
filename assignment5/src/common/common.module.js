5e7393e8be026387ff8aa19003fa8ba14783ed8f
Brian p Zaide <brianpzaide@gmail.com>
2019-01-22 07:07:06 +0530

    assignment5  fullstack-course5 --http to https.

--- assignment5/src/common/common.module.js
+++ assignment5/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'http://brianpzaide-course5.herokuapp.com')
+.constant('ApiPath', 'https://brianpzaide-course5.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
